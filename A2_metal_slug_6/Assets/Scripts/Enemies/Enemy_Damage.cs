dd3a9c7fbde08e1e6b5b32703a9211cd1c06e988
pprrathamagarwal <prathamagrawal76@gmail.com>
2024-09-21 03:49:42 +0530

    Enemy_Damage added

--- A2_metal_slug_6/Assets/Scripts/Enemies/Enemy_Damage.cs
+++ A2_metal_slug_6/Assets/Scripts/Enemies/Enemy_Damage.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Enemy_Damage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float damage;
 
-    // Update is called once per frame
-    void Update()
+     private void OnTriggerEnter2D(Collider2D collision) 
     {
-        
+        if(collision.tag=="Player")
+        {
+            collision.GetComponent<Player_Health>().TakeDamage(damage);
+        }    
     }
 }
